10a98b8959d79d7814b7b3afee3a3b785316ed43
Valentina BM <valentina15bermo@gmail.com>
2026-07-09 20:19:53 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,15 @@
 
 // loop 1 to 100
 
+// for (let i = 1; i <= 100; i++){
+//     console.log(i);
+// }
+
+// divisible by 3
+
 for (let i = 1; i <= 100; i++){
-    console.log(i);
+    if (i % 3 === 0){
+    console.log("Fizz");
+    }
 }
+
