73159dd148f727224bc8e43183fd99c518b8ffb0
Valentina BM <valentina15bermo@gmail.com>
2026-07-09 23:56:32 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,30 +1,51 @@
 // ALAB LOOPS
 
+// part 1 
 // loop 1 to 100
 
-for (let i = 1; i <= 100; i++){
-    // console.log(i);
+// for (let i = 1; i <= 100; i++){
+//     // console.log(i);
 
-// divisible by 3 and 5 
+// // divisible by 3 and 5 
 
-    if (i % 3 === 0 && i % 5 === 0){
-    console.log("Fizz Buzz");
+//     if (i % 3 === 0 && i % 5 === 0){
+//     console.log("Fizz Buzz");
+//     }
+
+// // divisible by 3
+
+// else if (i % 3 === 0){
+//     console.log("Fizz");
+//     }
+
+// // divisible by 5
+// else if (i % 5 === 0){
+//     console.log("Buzz");
+//     }
+
+// // not divisible by 3 or 5 
+
+// else {
+//     console.log(i);
+// }
+// }
+
+// part 2 
+let n = 9;
+
+while (true) {
+    n++;
+
+    let isPrime = true;
+
+    for(let i = 2; i < n; i++){
+        if(n % 1 === 0) {
+            isPrime = false;
+            break;
+        }
     }
-
-// divisible by 3
-
-else if (i % 3 === 0){
-    console.log("Fizz");
+    if(isPrime) {
+        console.log(n);
+        break;
     }
-
-// divisible by 5
-else if (i % 5 === 0){
-    console.log("Buzz");
-    }
-
-// not divisible by 3 or 5 
-
-else {
-    console.log(i);
-}
 }
